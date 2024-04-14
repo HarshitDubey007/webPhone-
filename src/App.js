e8cb18f99133e3ce69c5d4db56645122607a7fa3
HarshitDubey007 <harshitdubey1996@gmail.com>
2024-04-14 11:40:22 +0530

    webphone

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Softphone from './sipConnection';
+
+function App() {
+  return (
+    <div className="App">
+      <Softphone />
+    </div>
+  );
+}
+
+export default App;
